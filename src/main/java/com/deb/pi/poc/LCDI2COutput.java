de38ae7ec7751e09d52acbb2570dacc1b89c3d43
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2016-12-19 19:15:35 +0530

    new content

--- src/main/java/com/deb/pi/poc/LCDI2COutput.java
+++ src/main/java/com/deb/pi/poc/LCDI2COutput.java
@@ -1,18 +1,53 @@
 package com.deb.pi.poc;
 
-import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-import com.pi4j.io.i2c.I2CBus;
-import com.pi4j.io.i2c.I2CDevice;
-import com.pi4j.io.i2c.I2CFactory;
-import com.pi4j.io.i2c.I2CFactory.UnsupportedBusNumberException;
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.RaspiPin;
 
 public class LCDI2COutput {
 
-	public static void main(String[] args) throws UnsupportedBusNumberException, IOException, InterruptedException {
-		I2CBus bus = I2CFactory.getInstance( I2CBus.BUS_1 );
-		I2CDevice device = bus.getDevice( 0x27 );
+	public static void main(String[] args) throws Exception {
 		
-		device.write( (byte)1 );
+		GpioController gpio = GpioFactory.getInstance();
+		
+		GpioPinDigitalOutput out = gpio.provisionDigitalOutputPin( RaspiPin.GPIO_00 );
+		GpioPinDigitalInput in = gpio.provisionDigitalInputPin( RaspiPin.GPIO_14 );
+		out.high();
+	
+		String output = executeCommand( "python /home/pi/temp/examples.py blablabla 1" );
+		System.out.println( output );
+		System.out.println( in.getState() );
+		
+		Thread.sleep( 500 );
+		out.low();
+	}
+	
+	private static String executeCommand(String command) {
+
+		StringBuffer output = new StringBuffer();
+
+		Process p;
+		try {
+			p = Runtime.getRuntime().exec(command);
+			p.waitFor();
+			BufferedReader reader =
+                            new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+                        String line = "";
+			while ((line = reader.readLine())!= null) {
+				output.append(line + "\n");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return output.toString();
+
 	}
 }
