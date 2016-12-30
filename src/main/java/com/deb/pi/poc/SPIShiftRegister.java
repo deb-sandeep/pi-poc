6489452efec78dc075ee61a4a8680d251966de5b
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2016-12-30 18:04:28 +0530

    SPI poc with the 74hc595

--- src/main/java/com/deb/pi/poc/SPIShiftRegister.java
+++ src/main/java/com/deb/pi/poc/SPIShiftRegister.java
@@ -6,11 +6,34 @@ import com.pi4j.io.spi.SpiFactory;
 
 public class SPIShiftRegister {
 
+	private SpiDevice shiftRegister = null ; 
+			
+	public SPIShiftRegister() throws Exception {
+		shiftRegister = SpiFactory.getInstance( SpiChannel.CS0 ) ;
+	}
+	
+	public void runTest() throws Exception {
+		clear();
+		for( int i=0; i<256; i++ ) {
+			System.out.println( "Writing " + i ) ;
+			write( i ) ;
+			Thread.sleep( 100 ) ;
+			clear() ;
+		}
+		clear() ;
+	}
+	
+	private void clear() throws Exception {
+		write( 0 ) ;
+	}
+	
+	private void write( int i ) throws Exception {
+		shiftRegister.write( (byte)i ) ;
+	}
+
 	public static void main(String[] args) throws Exception {
-		
-		SpiDevice shiftRegister = SpiFactory.getInstance(SpiChannel.CS0);
-		
-		shiftRegister.write( (byte)0b10101010 );
-		Thread.sleep( 10000 );
+
+		SPIShiftRegister driver = new SPIShiftRegister() ;
+		driver.runTest() ;
 	}
 }
